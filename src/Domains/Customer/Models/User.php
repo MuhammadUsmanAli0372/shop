099cf61f626f30f40bdc2945fd8267b7312a73b5
Muhammad Usman Ali <usmanalimuhammad513@gmail.com>
2022-08-06 01:41:21 +0500

    Data modelling users and locations

---
+++ src/Domains/Customer/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domains\Customer\Model;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Domains\Customer\Model\Concerns\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasUuid;
+    use Notifiable;
+    use HasFactory;
+    use HasApiTokens;
+
+    protected $fillable = [
+        'uuid',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
