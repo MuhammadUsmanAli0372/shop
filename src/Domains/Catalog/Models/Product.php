e5a3412c52770fa768b75c695f697661248b8828
Muhammad Usman Ali <usmanalimuhammad513@gmail.com>
2022-08-06 14:12:00 +0500

    Model builders with shared trait scope for active

--- src/Domains/Catalog/Models/Product.php
+++ src/Domains/Catalog/Models/Product.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Domains\Catalog\Models;
 
 use Database\Factories\ProductFactory;
+use Domains\Catalog\Models\Builders\ProductBuilder;
 use Domains\Shared\Models\Concerns\HasKey;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -30,6 +33,23 @@ class Product extends Model
     protected $cast = [
         'active' => 'boolean',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function range(): BelongsTo
+    {
+        return $this->belongsTo(Range::class);
+    }
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new ProductBuilder(
+            query: $query,
+        );
+    }
 
     protected static function newFactory(): Factory
     {
--- src/Domains/Catalog/Models/Range.php
+++ src/Domains/Catalog/Models/Range.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Domains\Catalog\Models;
 
 use Database\Factories\RangeFactory;
+use Domains\Catalog\Models\Builders\RangeBuilder;
 use Domains\Shared\Models\Concerns\HasKey;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Range extends Model
 {
@@ -27,6 +30,18 @@ class Range extends Model
     protected $cast = [
         'active' => 'boolean',
     ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new RangeBuilder(
+            query: $query,
+        );
+    }
 
     protected static function newFactory(): Factory
     {
