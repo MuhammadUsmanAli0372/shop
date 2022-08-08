75c1db5348cde80dd7862573bec452d9065f297d
Muhammad Usman Ali <usmanalimuhammad513@gmail.com>
2022-08-08 21:03:19 +0500

    Lets get pesty

---
+++ tests/Pest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Tests\TestCase;
+
+uses(TestCase::class)->in('Feature');
+
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
+
+
+function something()
+{
+    // ..
+}
